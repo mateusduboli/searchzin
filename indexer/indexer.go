73e875692e6d8181f44863e03f1cf7dbe62f02a5
Mateus Dubiela Oliveira <mateusduboli@gmail.com>
2017-10-01 18:43:04 -0300

    Write documents to files

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -1,9 +1,41 @@
 package indexer
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"os"
+	"path"
+)
+
+const (
+	DATA_FOLDER = "data"
 )
 
 func IndexDocument(document Document) {
-	fmt.Printf("The document is [%s]\n", document)
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+
+	dir := path.Dir(ex)
+	dataFolder := path.Join(dir, DATA_FOLDER)
+	log.Printf("Data directory [%s]\n", dataFolder)
+	filename := path.Join(dataFolder, document.id)
+	log.Printf("Document file [%s]\n", filename)
+
+	if err := os.MkdirAll(dataFolder, 0755); err != nil {
+		panic(err)
+	}
+
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	b, err := json.Marshal(document)
+	if err != nil {
+		panic(err)
+	}
+	if _, err := f.Write(b); err != nil {
+		panic(err)
+	}
 }
