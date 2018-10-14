af12734e2bec087dddf18c56a848616899b7c5e7
Mateus Dubiela Oliveira <mateusduboli@gmail.com>
2018-10-14 23:16:28 +0200

    Generate contributors and changes automatically

--- scripts/release-body.sh
+++ scripts/release-body.sh
@@ -12,20 +12,30 @@ Examples:
     v1.1.0
   $0 v1.0.1 # Generates a message with the commits between v1.0.0..v1.0.1
 "
-if [ "$#"-ne "1" ]; then
+if [ "$#" -ne "1" ]; then
   echo $USAGE
   exit 1
 fi
 
 VERSION="$1"
 
-if [[ ! "$VERSION" =~ v[0-9]+\.[0-9]+\.[0.9]+ ]]; then
-  echo "Version must match semver $VERSION"
+if [[ ! "$VERSION" =~ v[0-9]+\.[0-9]+\.[0-9]+ ]]; then
+  echo "Version doesn't matches semver $VERSION"
   exit 1
 fi
 
 PREVIOUS_TAG=$(git tag --sort=v:refname | grep -B 1 $VERSION | head -n1)
 
-for C_MSG in $(git log $PREVIOUS_TAG..$VERSION); do
-  echo "* $C_MSG"
+IFS=$'\n' # It will use n instead of newlines without the $
+
+echo "## Changes"
+for COMMIT in $(git log --pretty=format:%s $PREVIOUS_TAG..$VERSION); do
+  echo "* $COMMIT"
+done
+
+echo "## Contributors"
+
+UNIQUE_AUTHORS=$(git log --pretty=format:"%an <%ae>" $PREVIOUS_TAG..$VERSION )
+for AUTHOR in $UNIQUE_AUTHORS; do
+  echo "* $AUTHOR"
 done
